7a92cca96712e46476adb086e9e364b0a0b8d266
ZhimaoLin <zhimaolin@outlook.com>
2023-08-30 19:45:38 -0400

    Added config file

---
+++ config.py
@@ -0,0 +1,8 @@
+OPENAI_API_KEY = "your-open-api-key"
+PINECONE_API_KEY = "your-pinecone-api-key"
+PINECONE_ENVIRONMENT = "your-pinecone-environment-name"
+PINECONE_INDEX_NAME = "your-pinecone-index-name"
+EMBEDDING_MODEL = "text-embedding-model"
+
+SPLITTER_CHUNK_SIZE = 30
+SPLITTER_CHUNK_OVERLAP = 5
