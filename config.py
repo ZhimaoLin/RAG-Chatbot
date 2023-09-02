515bc99f173bd427921f7dafb200abb75853a2ee
ZhimaoLin <zhimaolin@outlook.com>
2023-09-01 22:31:26 -0400

    Updated config file

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ OPENAI_API_KEY = "your-open-api-key"
 PINECONE_API_KEY = "your-pinecone-api-key"
 PINECONE_ENVIRONMENT = "your-pinecone-environment-name"
 PINECONE_INDEX_NAME = "your-pinecone-index-name"
-EMBEDDING_MODEL = "text-embedding-model"
 
-SPLITTER_CHUNK_SIZE = 30
-SPLITTER_CHUNK_OVERLAP = 5
+EMBEDDING_MODEL = "text-embedding-ada-002"
+SPLITTER_CHUNK_SIZE = 100
+SPLITTER_CHUNK_OVERLAP = 25
+UPLOAD_BATCH_SIZE = 32
